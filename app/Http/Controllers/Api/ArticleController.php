26c465622230e7e92a96f2c962bac6df6f49033a
Olang Daniel <olangdan17@gmail.com>
2022-08-04 14:12:53 +0100

    fix

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -21,25 +21,25 @@ class ArticleController extends Controller
     public function getAll()
     {
         $articles = Article::orderBy("id", 'asc')->get();
-        return response($articles, 200);
+        return response()->json($articles, 200);
     }
 
     public function get(Request $request, $articleId)
     {
         $article = Article::where('id', $articleId)->firstOrFail();
-        return response($article, 200);
+        return response()->json($article, 200);
     }
 
     public function update(ArticleRequest $request, $articleId)
     {
         $data = $request->validated();
         $article = Article::where('id', $articleId)->firstOrFail()->update($data);
-        return response($article, 200);
+        return response()->json($article, 200);
     }
 
     public function delete(Request $request, $articleId)
     {
         Article::where('id', $articleId)->firstOrFail()->delete();
-        return response("Successful", 200);
+        return response()->json("Successful", 200);
     }
 }
